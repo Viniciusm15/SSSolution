0bab8332cef62cf59cf7609453bcdbfc1d8b2268
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2020-02-28 14:53:27 -0300

    Cardinalidade ajustada.

--- MVCWebPresentationLayer/Models/ProdutoQueryViewModel.cs
+++ MVCWebPresentationLayer/Models/ProdutoQueryViewModel.cs
@@ -14,7 +14,9 @@ namespace MVCWebPresentationLayer.Models
         public double Preco { get; set; }
         public Cor Cor { get; set; }
         public bool VaiPilha { get; set; }
-        public virtual ICollection<FornecedorDTO> Fornecedores { get; set; }
-        public virtual ICollection<CategoriaDTO> Categorias { get; set; }
+        ublic int? FornecedorID { get; set; }
+        public virtual FornecedorDTO Fornecedor { get; set; }
+        public int? CategoriaID { get; set; }
+        public virtual CategoriaDTO Categoria { get; set; }
     }
 }
